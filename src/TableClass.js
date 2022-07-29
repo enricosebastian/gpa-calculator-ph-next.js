f9bdccaa676dfeda174931ed4d524772ebcbabf5
enricosebastian <enricosebastiansalazar@gmail.com>
2022-07-29 18:42:21 +0800

    Added initial calculation feature

--- src/TableClass.js
+++ src/TableClass.js
@@ -9,9 +9,16 @@ class TableClass extends React.Component {
 
     render() {
         let tableRowClass = this.props.classes.map(c => <TableRowClass key={"TableRowClass_"+c.id} course={c} termNumber={this.props.termNumber} changeCourseName={this.props.changeCourseName} changeCourseCode={this.props.changeCourseCode} changeCourseUnit={this.props.changeCourseUnit} changeCourseGrade={this.props.changeCourseGrade}/>);
+        let totalCourseUnits = this.props.classes.map(c => parseFloat(c.courseUnit)).reduce((prevValue,currentValue) => prevValue + currentValue, 0);
+        let gpa = this.props.classes.map(c => parseFloat(c.courseGrade)).reduce((prevValue,currentValue) => prevValue + currentValue, 0);
         return(
             <React.Fragment>
                 {tableRowClass}
+                total unit: {totalCourseUnits}
+                <br/>
+                gpa: {gpa}
+                
+                <br/>
                 <button onClick={this.addClass}>add</button>
             </React.Fragment>
         );

a334c7026fac82b845e738f978a853d78479333d
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-03-31 18:33:16 +0800

    Added more tailwind css stylings at header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <div>
-            Header
+        <div className="bg-blue-500 flex flex-col content-center items-center">
+            <div className="flex-initial grow bg-pink-500">GPA Calculator</div>
         </div>
     );
 }

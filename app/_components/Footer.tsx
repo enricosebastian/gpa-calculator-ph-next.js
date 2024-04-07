1de4cc0f13ba09e2a3e25a2d06038b3714105f92
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-04-07 14:19:30 +0800

    Added styling options for small screens

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <div  className="flex flex-row px-3">
-            <div id="links" className="grow">
-                <Link href="/about" className="mr-5 hover:font-bold">
+        <div className="flex flex-row md:px-3">
+            <div id="links" className="md:grow">
+                <Link href="/about" className="md:mr-5 mr-2 hover:font-bold">
                     //about
                 </Link>
-                <Link href="/contact_us" className="mr-5 hover:font-bold">
+                <Link href="/contact_us" className="md:mr-5 mr-2 hover:font-bold">
                     //contact_us
                 </Link>
                 <Link href="/add_your_university" className="hover:font-bold">
                     //add_your_university
                 </Link>
             </div>
-            
-            <div id="copyright" className="">
+
+            <div id="copyright" className="md:block hidden">
                 <span>
                     by <Link href="https://github.com/enricosebastian" className="hover:font-bold">@enricosebastian</Link>
                 </span>
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
-        <div className="h-1/5 flex justify-center items-center">
-            <div className="md:p-8 p-2 text-4xl border-solid border-black border-4 text-center">DLSU GPA CALCULATOR</div>
+        <div className="md:h-1/5 md:p-0 py-5 flex justify-center items-center">
+            <div className="md:p-8 p-2 text-4xl border-solid border-black border-4 text-center">
+                DLSU GPA CALCULATOR
+                <div className="md:hidden block text-base">by enricosebastian</div>
+            </div>
         </div>
     );
 }

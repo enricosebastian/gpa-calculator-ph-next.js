9feccb008f407ca9024030bbc664d1bd58ed954a
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-04-07 14:57:29 +0800

    made header link back to home

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className="md:h-1/5 md:p-0 py-5 flex justify-center items-center">
             <div className="md:p-8 p-2 text-4xl border-solid border-black border-4 text-center">
-                DLSU GPA CALCULATOR
+                <Link href="/">DLSU GPA CALCULATOR</Link>
                 <div className="md:hidden block text-base">by enricosebastian</div>
             </div>
         </div>

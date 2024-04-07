e52b5f224a875e464d6da87c11ba808134c84e6e
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-04-07 14:49:47 +0800

    started adding content for different pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import Header from "./_components/Header";
-import Content from "./Content";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
@@ -15,7 +14,9 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     <html lang="en">
         <body className="flex h-screen flex-col">
           <Header></Header>
-          <Content></Content>
+          <div className="flex-grow">
+            {children}
+          </div>
           <Footer></Footer>
         </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Header from "./_components/Header";
-import Footer from "./_components/Footer";
-import Content from "./Content";
-
 export default function Home() {
   return (
     <>
-      <Header></Header>
-      <Content></Content>
-      <Footer></Footer>
+      this is content
     </>
   );
 }

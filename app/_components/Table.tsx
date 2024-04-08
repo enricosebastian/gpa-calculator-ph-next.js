fecf2c476fb6428dcde0a4425c0d3d9ad60801fe
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-04-08 23:52:59 +0800

    Created _data folder and initialize functions

--- app/_components/Table.tsx
+++ app/_components/Table.tsx
@@ -3,45 +3,29 @@ import RowTitles from "./_table_components/RowTitles";
 import RowInputs from "./_table_components/RowInputs";
 import RowResults from "./_table_components/RowResults";
 
+import InitializeData from "../_data/initialize";
+
 import Term from "../_models/Term";
 import Course from "../_models/Course";
 
-function InitializeData(): Term[] {
-  const demo_course: Course = {
-    course_code: "GERPHIS",
-    course_title: "Philippine History",
-    grade: 3.5,
-    unit: 3.0,
-  }
-
-  const courses: Course[] = [demo_course];
-
-  const term_1: Term = {
-    term_name: "term_1",
-    courses: courses,
-  };
-
-  const terms: Term[] = [term_1];
-  return terms;
-}
+import { useState } from "react";
 
 export default function Table() {
   
   const terms: Term[] = InitializeData();
 
+  return (
+      <table className="table-fixed">
+        <thead>
+          <DropdownMenuForTerms></DropdownMenuForTerms>
+          <RowTitles></RowTitles>
+        </thead>
 
-    return (
-        <table className="table-fixed">
-          <thead>
-            <DropdownMenuForTerms></DropdownMenuForTerms>
-            <RowTitles></RowTitles>
-          </thead>
-
-          <tbody>
-            <RowInputs></RowInputs>
-            
-            <RowResults></RowResults>
-          </tbody>
-        </table>
-    );
+        <tbody>
+          <RowInputs></RowInputs>
+          
+          <RowResults></RowResults>
+        </tbody>
+      </table>
+  );
 }
---
+++ app/_data/initialize.ts
@@ -0,0 +1,34 @@
+import Term from "../_models/Term";
+import Course from "../_models/Course";
+
+export default function InitializeData(): Term[] {
+    const demo_course: Course = {
+        course_code: "GERPHIS",
+        course_title: "Philippine History",
+        grade: 3.5,
+        unit: 3.0,
+    }
+
+    const term_1_courses: Course[] = [demo_course];
+
+    const term_1: Term = {
+    term_name: "term_1",
+    courses: term_1_courses,
+    };
+
+    const terms: Term[] = [term_1];
+
+    for(let i = 2; i < 13; i++) {
+        const term_name = `term_${i}`;
+
+        const term_item: Term = {
+            term_name: term_name,
+            courses: [],
+        }
+        
+        terms.push(term_item);
+    }
+
+    
+    return terms;
+}

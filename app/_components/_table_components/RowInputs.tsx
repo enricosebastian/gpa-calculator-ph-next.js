a5f9aa6e60e3a3b2286a68833d1395bdab4322b5
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-04-16 00:05:55 +0800

    Removed redundant RowInput file when we have RowInputs

--- app/_components/_table_components/RowInputs.tsx
+++ app/_components/_table_components/RowInputs.tsx
@@ -1,9 +1,33 @@
+import Course from "@/app/_models/Course";
 import Term from "@/app/_models/Term";
-import RowInput from "./RowInput";
 
 interface Props {
     term: Term;
 }
+
+interface RowInputProps {
+    course: Course | null;
+}
+function RowInput({course}: RowInputProps) {
+    
+    return (
+        <>
+            <td className="border-black border-solid border-[5px]">
+                <input className="pl-1" value={course?.course_code}></input>
+            </td>
+            <td className="border-black border-solid border-[5px]">
+                <input className="pl-1" value={course?.course_title}></input>
+            </td>
+            <td className="border-black border-solid border-[5px]">
+                <input className="pl-1" value={course?.grade}></input>
+            </td>
+            <td className="border-black border-solid border-[5px]">
+                <input className="pl-1" value={course?.unit}></input>
+            </td>
+        </>
+    );
+}
+
 
 export default function RowInputs({term}: Props) {
     const inputData = (term.courses.length > 0) ? term.courses.map(course => <RowInput key={term.name+course.course_code} course={course}/>) : <RowInput course={null}/>;

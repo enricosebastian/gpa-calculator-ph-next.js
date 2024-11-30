e50fae3e0ff80dfc15d3dddee60f1de14b874097
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-11-30 21:02:09 +0800

    Modified excel sheet class to clean up code

--- _types/ExcelSheet.ts
+++ _types/ExcelSheet.ts
@@ -15,31 +15,83 @@ export class ExcelSheet {
 
     async initialize(file: Blob): Promise<void> {
         this._workbook = await this.readAndValidateFile(file);
-        
-        console.log(this._workbook);
 
-        const terms: Term[] = this.convertSheetsToTerms();
-        const courses: Course[] = this.convertSheetsToCourses();
+        if (this._workbook === null) {
+            throw new Error('Workbook is still null or undefined even after initialization. Try uploading a different file');
+        }
+
+        const workbook = this._workbook;
+
+        if(!this.workBookIsValidated(workbook)) {
+            throw new Error('Workbook validation failed. Try a different file');
+        }
+        
+        const terms: Term[] = this.convertSheetsToTerms(workbook);
+        const courses: Course[] = this.convertSheetsToCourses(workbook, terms);
+
+        console.log(terms);
+        console.log(courses);
     }
 
-    private convertSheetsToCourses(): Course[] {
+    private workBookIsValidated(workbook: XLSX.WorkBook): boolean {
+        workbook.SheetNames.forEach((sheet_name: string) => {
+            const sheet = workbook.Sheets[sheet_name];
+            const column_headers = XLSX.utils.sheet_to_json(sheet, {header: 1})[0] as string[];
+
+            if (column_headers.length < 4 || column_headers.length > 4) {
+                throw new Error(`You are missing a column! Check if you have four columns (course code, course name, grade, unit) for sheet "${sheet_name}"`);
+            }
+
+            const column_headers_lowercase = column_headers.map(column_header => column_header.toLowerCase());
+
+            if (!column_headers_lowercase.includes('course code') || !column_headers_lowercase.includes('grade') || !column_headers_lowercase.includes('unit') || !column_headers_lowercase.includes('course name'))  {
+                throw new Error(`You are missing a header! You need "course code", "grade", "unit", and "course name" as header names in your sheets. They don't need to be in any specific order, but you are missing one in sheet ${sheet_name}`);
+            }            
+        });
+
+        return true;
+    }
+
+    private convertSheetsToCourses(workbook: XLSX.WorkBook, terms: Term[]): Course[] {
         const courses: Course[] = [];
 
-        if (this._workbook === null)
-            return courses;
+        workbook.SheetNames.forEach((sheet_name: string) => {
+            const selected_term = terms.find(term => term.name === sheet_name);
 
-        this._workbook.SheetNames.forEach((sheet_name: string) => {
-            if (this._workbook === null)
-                return courses;
+            if (selected_term === undefined) {
+                throw new Error(`The sheet name ${sheet_name} does not exist in the terms[] array. That logic does not usually happen, so please try to fix your Excel sheet`);
+            }
 
-            const sheet = this._workbook.Sheets[sheet_name];
-            const column_headers = XLSX.utils.sheet_to_json(sheet, {header: 1, range: 'A1:D1'});
+            const sheet = workbook.Sheets[sheet_name];
+            const column_headers = XLSX.utils.sheet_to_json(sheet, {header: 1})[0] as string[];
 
-            if (column_headers.length < 4 || column_headers.length > 4) 
-                return;
+            const new_header_names = column_headers.map(header => {
+                switch (header.toLowerCase()) {
+                    case 'course code':
+                        return 'code';
+                    case 'course name':
+                        return 'name';
+                    default:
+                        return header.toLowerCase();
+                }
+            }); 
 
-            column_headers.forEach(column_header => {
-                console.log(column_header);
+            const row_data: Course[] = XLSX.utils.sheet_to_json(sheet, {
+                header: new_header_names,
+                range: 1, // Skip the original header row
+            }) as Course[];
+
+            row_data.forEach(row => {
+                const course: Course = {
+                    id: uuid(),
+                    name: row.name,
+                    code: row.code,
+                    unit: row.unit,
+                    grade: row.grade,
+                    term_id: selected_term.id
+                };
+
+                courses.push(course);
             });
             
         });
@@ -47,13 +99,10 @@ export class ExcelSheet {
         return courses;
     }
 
-    private convertSheetsToTerms(): Term[] {
+    private convertSheetsToTerms(workbook: XLSX.WorkBook): Term[] {
         const terms: Term[] = [];
 
-        if (this._workbook === null)
-            return terms;
-
-        this._workbook.SheetNames.forEach((sheet_name: string) => {
+        workbook.SheetNames.forEach((sheet_name: string) => {
             const term: Term = {
                 id: uuid(),
                 name: sheet_name
@@ -64,7 +113,6 @@ export class ExcelSheet {
 
         return terms;
     }
-
 
     private async readAndValidateFile(file: Blob): Promise<WorkBook> {
         return new Promise((resolve, reject) => {

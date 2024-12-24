60fbfe55754533172eb3e6343dd8af0c66d04e8b
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-12-24 05:12:59 -0500

    Revised styling of the name container to make it more fit

--- _components/RetroTable/RetroTable.tsx
+++ _components/RetroTable/RetroTable.tsx
@@ -1,5 +1,13 @@
 export default function RetroTable() {
     return (
-        <div className="retrotable--container">table</div>
+        <div className="retrotable--container">
+            <div className="retrotable--content">
+                <div className="retrotable--name--container">
+                    <input className="retro--input" type="text"></input>
+                </div>
+                <div className="retrotable--table--container"></div>
+                <div className="retrotable--footer--container"></div>
+            </div>
+        </div>
     );
 }
--- _components/Sidebar/Sidebar.tsx
+++ _components/Sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
 export default function Sidebar() {
     return (
         <>
-            <div className="sidebar--container--1">
-                sidebar1
+            <div className="sidebar--container sidebar--container--1">
+                <div className="sidebar--content"></div>
             </div>
             
 
-            <div className="sidebar--container--2">
-                sidebar2
+            <div className="sidebar--container sidebar--container--2">
+                <div className="sidebar--content"></div>
             </div>
         
         </>

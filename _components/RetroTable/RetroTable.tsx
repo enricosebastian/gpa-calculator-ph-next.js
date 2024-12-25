c45cffc45eb9f8fc1976164b869f6ca6e918fa5d
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2024-12-25 04:57:22 -0500

    Created separate sass modules for specific components

--- _components/RetroTable/RetroTable.tsx
+++ _components/RetroTable/RetroTable.tsx
@@ -1,3 +1,5 @@
+import styles from './RetroTable.module.scss';
+
 export default function RetroTable() {
     return (
         <div className="retrotable--container">
@@ -5,9 +7,36 @@ export default function RetroTable() {
                 <div className="retrotable--term--name--container">
                     <input className="retro--input" type="text"></input>
                 </div>
-                <div className="retrotable--table--container"></div>
+                <div className="retrotable--table--container">
+                    <Table/>
+                </div>
                 <div className="retrotable--footer--container"></div>
             </div>
         </div>
+    );
+}
+
+
+function Table() {
+    return (
+        <table className={styles.retrotable}>
+            <thead>
+                <tr>
+                    <th>course</th>
+                    <th>course code</th>
+                    <th>grade</th>
+                    <th>unit</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th>course</th>
+                    <th>course code</th>
+                    <th>grade</th>
+                    <th>unit</th>
+                </tr>
+            </tbody>
+        </table>
     );
 }

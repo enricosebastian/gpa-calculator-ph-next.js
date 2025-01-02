8157cc54fff4a7fb6156e820e6c3564e2c03bf95
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-01-02 00:40:37 -0500

    Added sidebar styling

--- _components/Sidebar/Sidebar.tsx
+++ _components/Sidebar/Sidebar.tsx
@@ -1,13 +1,19 @@
+import styles from './Sidebar.module.scss';
+
 export default function Sidebar() {
     return (
         <>
-            <div className="sidebar--container sidebar--container--1">
-                <div className="sidebar--content"></div>
+            <div className={`${styles.sidebar_container} ${styles.sidebar_1}`}>
+                <div className={styles.sidebar_content}>
+                    <div className={styles.sidebar_row}>your gpa is: <div className={styles.score_card}>4.000</div></div>
+                    <div className={styles.sidebar_row}>your cgpa is: <div className={styles.score_card}>4.000</div></div>
+
+                </div>
             </div>
             
 
-            <div className="sidebar--container sidebar--container--2">
-                <div className="sidebar--content"></div>
+            <div className={`${styles.sidebar_container} ${styles.sidebar_2}`}>
+                <div className={styles.sidebar_content}></div>
             </div>
         
         </>

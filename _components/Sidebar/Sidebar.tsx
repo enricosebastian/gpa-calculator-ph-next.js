b7459a3cd6d39575aa775e3647c56c427156a9a2
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-01-14 23:44:21 +0800

    Added initial style for the select dropdown

--- _components/Sidebar/Sidebar.tsx
+++ _components/Sidebar/Sidebar.tsx
@@ -1,19 +1,23 @@
 import styles from './Sidebar.module.scss';
 
 export default function Sidebar() {
+    const colleges = ['DLSU', 'AdMU', 'UST'];
+    const college_select_fields = colleges.map(college => <option value={college}>{college}</option>)
     return (
         <>
             <div className={`${styles.sidebar_container} ${styles.sidebar_1}`}>
                 <div className={styles.sidebar_content}>
                     <div className={styles.sidebar_row}>your gpa is: <div className={styles.score_card}>4.000</div></div>
                     <div className={styles.sidebar_row}>your cgpa is: <div className={styles.score_card}>4.000</div></div>
-
                 </div>
             </div>
             
 
             <div className={`${styles.sidebar_container} ${styles.sidebar_2}`}>
-                <div className={styles.sidebar_content}></div>
+                <div className={styles.sidebar_content}>
+                    <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{college_select_fields}</select></div>
+                    <div className={styles.sidebar_row}>your cgpa is: <div className={styles.score_card}>4.000</div></div>
+                </div>
             </div>
         
         </>

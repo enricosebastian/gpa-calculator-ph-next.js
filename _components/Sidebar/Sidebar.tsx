654393c71730ab85417cbd17cc2273c9a5626875
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-01-15 15:59:07 +0800

    Made sure dropdown fie for terms works

--- _components/Sidebar/Sidebar.tsx
+++ _components/Sidebar/Sidebar.tsx
@@ -1,8 +1,18 @@
+import { useMainContext } from '@/_context/MainContext';
 import styles from './Sidebar.module.scss';
+import { useTermContext } from '@/_context/TermContext';
+import { Tenali_Ramakrishna } from 'next/font/google';
 
 export default function Sidebar() {
     const colleges = ['DLSU', 'AdMU', 'UST'];
-    const college_select_fields = colleges.map(college => <option value={college}>{college}</option>)
+    const college_select_fields = colleges.map(college => <option key={college} value={college}>{college}</option>)
+
+    const {selectedTermId} = useMainContext();
+    const {terms} = useTermContext();
+    
+    const selectedTerm = terms.find(term => term.id === selectedTermId);
+    
+    const term_select_fields = terms.map(term => <option key={term.id} value={term.id}>{term.name}</option>);
     return (
         <>
             <div className={`${styles.sidebar_container} ${styles.sidebar_1}`}>
@@ -16,7 +26,7 @@ export default function Sidebar() {
             <div className={`${styles.sidebar_container} ${styles.sidebar_2}`}>
                 <div className={styles.sidebar_content}>
                     <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{college_select_fields}</select></div>
-                    <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{college_select_fields}</select><button className={styles.sidebar_button}>+</button><button className={styles.sidebar_button}>-</button></div>
+                    <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{term_select_fields}</select><button className={styles.sidebar_button}>+</button><button className={styles.sidebar_button}>-</button></div>
                 </div>
             </div>
         

69786aa55c82b9e7712ca1b0360b76b5f65fbfe7
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-01-15 15:54:04 +0800

    Added add and delete term buttons

--- _components/RetroTable/RetroTable.tsx
+++ _components/RetroTable/RetroTable.tsx
@@ -22,7 +22,9 @@ export default function RetroTable() {
                 <div className="retrotable--table--container">
                     <Table/>
                 </div>
-                <div className="retrotable--footer--container"></div>
+                <div className="retrotable--footer--container">
+                    footer
+                </div>
             </div>
         </div>
     );
--- _components/Sidebar/Sidebar.tsx
+++ _components/Sidebar/Sidebar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar() {
             <div className={`${styles.sidebar_container} ${styles.sidebar_2}`}>
                 <div className={styles.sidebar_content}>
                     <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{college_select_fields}</select></div>
-                    <div className={styles.sidebar_row}>your cgpa is: <div className={styles.score_card}>4.000</div></div>
+                    <div className={styles.sidebar_row}><select className={styles.sidebar_select}>{college_select_fields}</select><button className={styles.sidebar_button}>+</button><button className={styles.sidebar_button}>-</button></div>
                 </div>
             </div>
         

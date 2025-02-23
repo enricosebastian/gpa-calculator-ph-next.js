e102914d1ceb37992d77c31bf877eedf326deabc
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-02-23 12:01:56 +0800

    Added mobile mode for better compatibility

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,46 @@ import Toolbar from "@/_components/Toolbar/Toolbar";
 import Table from "@/_components/Table/Table";
 import Sidebar from "@/_components/Sidebar/Sidebar";
 import Main from "@/_components/Main/Main";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const content = isMobile? <div>mobile_mode</div> : showDesktopMode();
+
   return (
     <CourseContextProvider>
       <TermContextProvider>
         <MainContextProvider>
-          <div className="main--header">PH UNIVERSITY GPA ESTIMATOR</div>
-          <div className="main--viewport">
-            <div className="main--content">
-              <Sidebar/>
-              <Main/>
-            </div>
-          </div>
+          {content}
         </MainContextProvider>
       </TermContextProvider>
     </CourseContextProvider>
+  );
+}
+
+function showDesktopMode() : ReactNode {
+  return (
+    <>
+      <div className="main--header">PH UNIVERSITY GPA ESTIMATOR</div>
+      <div className="main--viewport">
+        <div className="main--content">
+          <Sidebar/>
+          <Main/>
+        </div>
+      </div>
+    </>
   );
 }

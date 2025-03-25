88fb52367986b579c0c0509133c975e3196f54f4
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-03-25 23:35:07 +0800

    Implemented college theme changer feature

--- _components/CompactMain/CompactMain.tsx
+++ _components/CompactMain/CompactMain.tsx
@@ -1,25 +1,43 @@
-import { Orientation } from "@/_types/Enums";
+import { Orientation, University } from "@/_types/Enums";
 import { RetroDiv, RetroDivSubComponent, RetroDivButton } from "../RetroDiv/RetroDiv";
 import RetroDropdown from "../RetroDropdown/RetroDropdown";
 import { useMainContext } from "@/_context/MainContext";
 import { useTermContext } from "@/_context/TermContext";
 import { useCourseContext } from "@/_context/CourseContext";
-import { MouseEventHandler, ReactNode } from "react";
+import { ChangeEvent, MouseEventHandler, ReactNode } from "react";
 import { Course } from "@/_types/Course";
+import { useTheme } from "next-themes";
 
 
 export default function CompactMain() {
-    const {selectedCourses, selectedTermId, setSelectedTermId} = useMainContext();
+    const {selectedCourses, selectedTermId, setSelectedTermId, university, setUniversity} = useMainContext();
     const {terms, addTerm, modifyTerm, deleteTerm} = useTermContext();
     const {courses, addCourse, deleteCourse} = useCourseContext();
 
+    const {theme, setTheme} = useTheme();
+
+    const colleges: string[] = Object.keys(University).filter(university => university !== 'NONE');
+    const college_select_fields = colleges.map(college => <option key={college} value={college}>{college}</option>)
+
     const retroDivCourses = selectedCourses.map(course => <RetroDivCourse key={course.code} course={course}/>);
+
+    const handleCollegeSelected = (e: ChangeEvent<HTMLSelectElement>) => {
+        const new_selected_university: University = e.target.value as University;
+        
+        if (!new_selected_university) {
+            throw new Error('University does not exist!');
+        }
+
+        setTheme(new_selected_university);
+        setUniversity(new_selected_university);
+    };
+    
 
     return (
         <>
             <RetroDiv>
                 <RetroDivSubComponent orientation={Orientation.TOP}>
-                    <RetroDropdown><option>dlsu_gpa</option></RetroDropdown>
+                    <RetroDropdown onChange={e => handleCollegeSelected(e)}>{college_select_fields}</RetroDropdown>
                 </RetroDivSubComponent>
 
                 <RetroDivSubComponent orientation={Orientation.BOTTOM}>

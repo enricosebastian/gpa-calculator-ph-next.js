1a36bcee941b8f10228b92c9ee1ea0081effefa1
enricosebastian <74126864+enricosebastian@users.noreply.github.com>
2025-03-25 21:56:09 +0800

    Added styling for component and box

--- _components/RetroDiv/RetroDiv.tsx
+++ _components/RetroDiv/RetroDiv.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import styles from './RetroDiv.module.scss';
+import RetroDropdown from '../RetroDropdown/RetroDropdown';
 
 export default function RetroDiv() {
 
     return (
         <div className={styles.retro_div}>
-            <div className={`${styles.box} ${styles.bottom} ${styles.left}`}>this is a test</div>
+            <div className={`${styles.box} ${styles.left}`}>+</div>
+            <div className={`${styles.box} ${styles.right}`}>-</div>
+            <div className={`${styles.component} ${styles.top}`}><RetroDropdown><option>test</option></RetroDropdown></div>
             this is the content in retro div!
         </div>
     );
